bb48e211165743a5ef2c5e4e1b93848cce5196c0
“ihsanunot” <ihsanunot@gmail.com>
2023-05-06 02:31:02 +0700

    dasar

---
+++ README.md
@@ -0,0 +1,17 @@
+# Latihan Node JS Dasar
+
+---
+**Cara Pakai:**
+> npm install
+
+habis itu buat run nya :
+> npm start
+
+1. Membuat Server
+2. Membuat Tampilan
+3. Jangan Lupa npm install terlebih dahulu
+4. port default di 3001, bisa diganti sesuka kamu.
+5. Nama database buah_daftar
+6. Nama Table nya buahan
+
+Ihsan.
---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'ihsanihsan',
+  database: 'buah_daftar'
+});
+
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM buahan',
+    (error, results) => {
+      res.render('index.ejs', {buahan: results});
+    }
+  );
+});
+
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/create', (req, res) => {
+  connection.query(
+    'INSERT INTO buahan (name) VALUES (?)',
+    [req.body.itemName],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+app.post('/delete/:id', (req, res) => {
+  connection.query(
+    'DELETE FROM buahan WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+app.get('/edit/:id', (req, res) => {
+  connection.query(
+    'SELECT * FROM buahan WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.render('edit.ejs', {item: results[0]});
+    }
+  );
+});
+
+app.post('/update/:id', (req, res) => {
+
+  connection.query(
+    'UPDATE buahan SET name = ? WHERE id = ?',
+    [req.body.itemName,req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+  
+});
+
+app.listen(3000);
